bf947628264eef8624984dbe1b891ca8636c391a
codemasher <smiley@chillerlan.net>
2018-10-24 06:17:36 +0200

    :sparkles: +includes/functions

---
+++ src/gamebrowser_functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @filesource   gamebrowser_functions.php
+ * @created      22.10.2018
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2018 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\GameBrowser;
+
+const GAMEBROWSER_INCLUDES = true;
+
---
+++ src/includes.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @filesource   includes.php
+ * @created      22.10.2018
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2018 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\GameBrowser;
+
+// @codeCoverageIgnoreStart
+if(!defined('GAMEBROWSER_INCLUDES')){
+	require_once __DIR__.'/gamebrowser_functions.php';
+}
+// @codeCoverageIgnoreEnd
+
+
+
